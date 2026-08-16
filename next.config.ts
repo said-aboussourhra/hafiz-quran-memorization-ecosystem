3eabeba91ce195bfc9fb054f8b2ade3bb5f9012d
said-aboussourhra <s01said@outlook.fr>
2026-08-16 16:16:53 +0100

    Fix: package.json and next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // إزالة eslint من هنا
   typescript: {
     ignoreBuildErrors: true,
   },
-  // eslint تم نقله إلى eslint.config.mjs
   serverExternalPackages: ['drizzle-orm', 'pg'],
   env: {
     DATABASE_URL: process.env.DATABASE_URL || '',
